c3ed875bd480e2eb4d5a9c9167bdb1c0c69d3dff
al285 <al285@njit.edu>
2015-04-18 12:40:32 -0400

    editing my pervious code

--- pro/index.php
+++ pro/index.php
@@ -9,24 +9,23 @@ public columnHeading;
 //Function
          
          public funtion readcsv($csvFiles , $columnHeading){
-         ini_set('auto_detect_line_endings, TRUE);
-             if (($handle =fopen($csv, "r")) ! == FALSE)
-             {
-
-               while (($row =fgetcsv($handle, 1000, ",")) ! == False){
-
-                          if ($column_heading == TRUE)
-                          {
-
-                            $column_heading = $row;
-      				         $column_headings = FALSE;
-   			} else {     
-       				        $record = array_combine($column_heading, $row);
-      				           $records[] = $record;
+         public static function readCSV($fname,$heading) {
+		$first_row = TRUE;
+		ini_set('auto_detect_line_endings',TRUE);
+		if (($handle = fopen($fname, "r")) !== FALSE) {
+    		while (($row = fgetcsv($handle, 1000, ",")) !== FALSE) {
+     		if($first_row == TRUE) {
+       		$column_heading = $row;
+       		$first_row = FALSE;
+      				    
+  	} else {     
+        $record = array_combine($column_heading, $row);
+         $records[] = $record;
     				                    }
    		}
     		fclose($handle);
 	  }
+	  return $records;
 		
 	                     foreach($records as $record) {
    	                      foreach($record as $key => $value) {
@@ -37,21 +36,46 @@ public columnHeading;
 	
 	}
 
-//CSV Files Reading
+//calling csv to read
 
     class newClass{
-            function csvcallingfile()  {
+            function csvcallfile()  {
 
                  $csvfile= new csvfile();
-                 $newcsv= $csvfile->readcsv ();
-                 echo $newcsv;
+                 $newcsvFil= $csvfile->readcsv();
+                 echo $newcsvFil;
+      
+      
+      
                  }
+                 
+                 else{
+		//echoing the key and value
+		$record = $records[$_GET['record']];
+		echo "<table border = 1 bordercolor= blue cellspacing=0 cellpadding=3 >";
+		echo "<tr>";
+		foreach($record as $key =>  $value) {
+			
+			echo "<th>$key</th>";
+			echo "<td>$value</td>";
+			
+		echo "</tr>";
+		
+	
+			
+		}
+		
+		echo "</table>";
+	
+	
+	}
+	}
 
 }
 
-
-   $newfile=new csvfile();
-   $newfile->readcsv("hd2013.csv", TRUE);
+//calling files 
+   $NF=new csvfile();
+   $NF->readCSV("hd2013.csv", TRUE);
    
 
 
